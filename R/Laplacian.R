42ae301539dde4d796aeea940633536ddba21977
CharlieCarpenter <charles.carpenter@cuanschutz.edu>
2023-03-21 12:31:55 -0600

    Including more laplacian options

--- R/Laplacian.R
+++ R/Laplacian.R
@@ -4,7 +4,7 @@
 #'
 #'
 #' @param A An n by n kernel matrix, where n is the sample size, that represents your initial adjacency matrix. Kernel matrices are symmetric, positive semi-definite distance matrices
-#' @param laplacian Either `"rw"` for the random walk laplacian (default), i.e., \eqn{I-D^{-1}A}, or `"sym"` for a symmetric Laplacian, i.e., \eqn{I-D^{-1/2}AD^{1/2}}
+#' @param laplacian One of `"shift"`, `"Ng"`, `"rw"` or `"sym"`. See details for description
 #' @param grf.type Type of graph to calculate: `"full"` for adjacency matrix equal to the kernel, `"knn"` for a k-nearest neighbors graph, `"e-graph"` for an "epsilon graph"
 #' @param k An integer value for `k` in the k-nearest neighbors graph. Only the `k` largest edges (most similar neighbors) will be kept
 #' @param rho A value for the dispersion parameter in the Gaussian kernel. It is in the denominator of the exponent, so higher values correspond to lower similarity. By default it is the median pairwise Gaussian distance
@@ -12,6 +12,7 @@
 #' @param mutual Make a "mutual" knn graph. Only keeps edges when two nodes are both in each others k-nearest set
 #' @param binary.grf Set all edges >0 to 1
 #' @param plots Whether or not to plot the final graph, a heatmap of calculated kernel, and the eigen values of the Laplacian
+#' @details The four Lapalacians are defined as \eqn{L_{shift}=I+D^{-1/2}AD^{-1/2}}, \eqn{L_{Ng}=D^{-1/2}AD^{-1/2}}, \eqn{L_{sym}=I-D^{-1/2}AD^{-1/2}}, and \eqn{L_{rw}=I-D^{-1}A}. The shifted Laplacian, \eqn{L_{shift}=I+D^{-1/2}AD^{-1/2}}, is recommended for multi-view spectral clustering.
 #' @return An n\eqn{\times}n matrix where `n` is the number of rows in `dat`.
 #' @references \url{https://academic.oup.com/bioinformatics/article/36/4/1159/5566508#199177546}
 #' @examples
@@ -26,7 +27,7 @@
 #'
 #' Laplacian(A, laplacian='rw', grf.type = 'knn')
 #' @export
-Laplacian <- function(A, laplacian = c('sym', 'rw'),
+Laplacian <- function(A, laplacian = c('shift', 'Ng', 'sym', 'rw'),
                       grf.type = c('full', 'knn', 'e-graph'),
                       k=5,
                       rho=NULL,
@@ -51,6 +52,20 @@ Laplacian <- function(A, laplacian = c('sym', 'rw'),
   if(binary.grf) A[A >0] <- 1
 
   deg <- rowSums(A)
+
+  ## 'Shifted' Laplacian
+  if(lap.type == "shift"){
+    ds <- ifelse(deg>0, 1/sqrt(deg), 0)
+    # L = I + D^(-1/2) A D^(-1/2)
+    L <- diag(nrow = nrow(A)) + diag(ds) %*% A %*% diag(ds)
+  }
+
+  ## Laplacian used by Ng (2002) 'On Spectral Clustering'
+  if(lap.type == "Ng"){
+    ds <- ifelse(deg>0, 1/sqrt(deg), 0)
+    # L = D^(-1/2) A D^(-1/2)
+    L <- diag(ds) %*% A %*% diag(ds)
+  }
 
   if(laplacian == 'sym'){
     ds <- ifelse(deg>0, 1/sqrt(deg), 0)
